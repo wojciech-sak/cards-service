43f60e2fda4a5a4eada346904dbfab3c2010eafc
wojciech-sak <wojciech.r.sak@gmail.com>
2021-11-07 19:35:01 +0100

    first commit

---
+++ src/test/java/com/kodilla/cards/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.cards.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
